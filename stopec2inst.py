74399c40f0229914e4ab2a50607219c935598268
nileshdevp <82369576+nileshdevp@users.noreply.github.com>
2023-02-22 15:44:53 +0530

    Update stopec2inst.py
    NameError: name 'client' is not defined

--- stopec2inst.py
+++ stopec2inst.py
@@ -4,7 +4,7 @@ import sys
 region = 'ap-northeast-1'
 accesskey = 'AKIASPARKNSUQU5OEYII'
 secretkey = '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'
-
+client = boto3.client('ec2',region_name=region,aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
 response = client.stop_instances(
     InstanceIds=[
         'i-06c09421fb364174c',
